93c65f904cf6301a89990c346aa0448794afc578
britt <britt.shroyer@gmail.com>
2020-02-12 06:50:10 -0600

    remove gitignore files

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,33 @@
 import App, { Container } from "next/app";
 import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
 class MyApp extends App {
+  // Next.js Hook => anything returned is exposed to the component via props
+  // For every page of app, this function will serve to crawl each page for queries and mutations. This will resolve those and return data
+  // before component is rendered
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // This exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
-export default MyApp;
+export default withData(MyApp);
