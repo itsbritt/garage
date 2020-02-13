f8503e7dd44dc2eafafb6e22bb3f4865292e4507
britt <britt.shroyer@gmail.com>
2020-02-13 12:20:33 -0600

    add deleteitem component along with mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -24,6 +24,13 @@ const Mutations = {
       },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // Check if they own item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import formatMoney from "../lib/formatMoney";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
+import DeleteItem from "./DeleteItem";
 
 export default class Item extends Component {
   static propTypes = {
@@ -30,7 +31,7 @@ export default class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
